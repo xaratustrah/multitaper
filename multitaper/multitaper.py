45f9e746d7d28e8b4a50c95d6534365502da43b0
xaratustrah <shahab.sanjari@gmail.com>
2018-12-12 11:53:28 +0100

    now using scipy's own dpss window

--- multitaper/multitaper.py
+++ multitaper/multitaper.py
@@ -2,61 +2,35 @@
 # −*− coding:utf-8 −*−
 
 import numpy as np
-from scipy.linalg import eigh_tridiagonal
+from scipy.signal.windows import dpss
 
 
-class Multitaper(object):
+def pmtm(signal, dpss, axis=-1):
     '''
-    A class to perform the spectral density estimation of an input signal based on the multitaper method
+    Estimate the power spectral density of the input signal.
+    signal: n-dimensional array of real or complex values
+    axis:   axis along which to apply the Slepian windows. Default is the last one.
     '''
+    # conversion to positive-only index
+    axis_p = (axis + signal.ndim) % signal.ndim
+    sig_exp_shape = list(signal.shape[:axis]) + [1] + list(signal.shape[axis:])
+    tap_exp_shape = [1] * axis_p + \
+        list(dpss.shape) + [1] * (signal.ndim - 1 - axis_p)
+    signal_tapered = signal.reshape(
+        sig_exp_shape) * dpss.reshape(tap_exp_shape)
+    return np.fft.fftshift(np.mean(np.absolute(np.fft.fft(signal_tapered, axis=axis_p + 1))**2, axis=axis_p), axes=axis_p)
 
-    def __init__(self, nn, nw=3.5, kk=7):
-        '''
-        Initialize the object.
-        nn:     taper length
-        nw:     half of resolution bandwidth
-        kk:     number of tapers, in principle less than 2*nw
-        '''
-
-        self.kk = kk
-        self.nw = nw
-        self.nn = nn
-        self.dpss = self._regen_dpss()
-
-    def __str__(self):
-        return "No. of tapers: {}, taper length: {}, half res. BW: {}".format(self.kk, self.nn, self.nw)
-
-    def _regen_dpss(self):
-        '''
-        The generated tapers in 2D array [[taper_0], [taper_1], ...] are ordered decreasingly by their respective eigenvalues.
-        '''
-        ww = self.nw / self.nn
-        diag_main = ((self.nn - 1) / 2 - np.arange(self.nn))**2 * np.cos(2 * np.pi * ww)
-        diag_off = np.arange(1, self.nn) * np.arange(self.nn - 1, 0, -1) / 2
-        vecs = eigh_tridiagonal(diag_main, diag_off, select='i', select_range=(self.nn - self.kk, self.nn - 1))[1]
-        # polarity follows Slepian convention
-        return (vecs * np.where(vecs[0, :] > 0, 1, -1)).T[::-1]
-
-    def estimate(self, signal, axis=-1):
-        '''
-        Estimate the power spectral density of the input signal.
-        signal: n-dimensional array of real or complex values
-        axis:   axis along which to apply the Slepian windows. Default is the last one.
-        '''
-        # conversion to positive-only index
-        axis_p = (axis + signal.ndim) % signal.ndim
-        sig_exp_shape = list(signal.shape[:axis]) + [1] + list(signal.shape[axis:])
-        tap_exp_shape = [1] * axis_p + list(self.dpss.shape) + [1] * (signal.ndim-1-axis_p)
-        signal_tapered = signal.reshape(sig_exp_shape) * self.dpss.reshape(tap_exp_shape)
-        return np.fft.fftshift(np.mean(np.absolute(np.fft.fft(signal_tapered, axis=axis_p+1))**2, axis=axis_p), axes=axis_p)
 
 # ------------------------
 
 
 if __name__ == '__main__':
-    mymtm = Multitaper(2048)
-    sig = np.vectorize(complex)(np.random.rand(2048), np.random.rand(2048))
-    print(mymtm.estimate(sig))
-    mymtm = Multitaper(256)
-    sig = np.reshape(sig, (8, 256))
-    print(mymtm.estimate(sig))
+    # a small test
+
+    mydpss = dpss(M=1024, NW=4, Kmax=6)
+    sig = np.vectorize(complex)(np.random.rand(1024), np.random.rand(1024))
+    print(pmtm(sig, mydpss))
+
+    mydpss = dpss(M=128, NW=4, Kmax=6)
+    sig = np.reshape(sig, (8, 128))
+    print(pmtm(sig, mydpss, axis=1))
